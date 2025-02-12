79a4f124743873d2ab7a289f5f5a479818eb2e9d
Braden-Mills <98986528+Braden-Mills@users.noreply.github.com>
2025-02-12 12:13:16 -0500

    Test 3

--- adventure.py
+++ adventure.py
@@ -92,10 +92,8 @@ def enter_dungeon(player_health, inventory, dungeon_rooms):
     for room in dungeon_rooms:
         room_description, item, challenge_type, challenge_outcome = room
         print(room_description)
-
         if item:
             inventory = acquire_item(inventory, item)
-        
         if challenge_type == "puzzle":
             print("You encounter a puzzle!")
             choice = input("Do you want to solve or skip the puzzle? ").lower()
